61ceb953eb657d533a53dec266be18f856104b49
Norberto Caceres <norberto.e.888@gmail.com>
2023-05-03 18:33:58 -0500

    install react navigation deps

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import "react-native-gesture-handler";
 import { useCallback, useEffect, useState } from "react";
 import { StyleSheet, Text } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
